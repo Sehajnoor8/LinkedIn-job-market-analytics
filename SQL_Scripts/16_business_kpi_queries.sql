2d31bf0d1d7172f83caf1d3480d6daff8b628319
Sehajnoor Kaur <sehajn8@gmail.com>
2026-06-01 16:48:05 +0530

    Implemented Business KPI Queries

---
+++ SQL_Scripts/16_business_kpi_queries.sql
@@ -0,0 +1,42 @@
+SELECT
+    company_name,
+    COUNT(job_id) AS total_openings
+FROM companies
+JOIN jobs
+ON companies.company_id = jobs.company_id
+GROUP BY company_name
+ORDER BY total_openings DESC;
+
+SELECT
+    company_name,
+    AVG(max_salary) AS avg_salary
+FROM companies
+JOIN jobs
+ON companies.company_id = jobs.company_id
+JOIN salaries
+ON jobs.job_id = salaries.job_id
+GROUP BY company_name
+ORDER BY avg_salary DESC;
+
+SELECT
+    skill_name,
+    COUNT(job_id) AS demand_count
+FROM skills
+JOIN job_skills
+ON skills.skill_id = job_skills.skill_id
+GROUP BY skill_name
+ORDER BY demand_count DESC;
+
+SELECT
+    remote_option,
+    COUNT(*) AS total_jobs
+FROM locations
+JOIN jobs
+ON locations.location_id = jobs.location_id
+GROUP BY remote_option;
+
+SELECT
+    experience_level,
+    COUNT(*) AS total_jobs
+FROM jobs
+GROUP BY experience_level;
